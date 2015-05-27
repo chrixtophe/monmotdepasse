ce7285d84bf734be65111a33da39dc755a2e8ae2
root <root@raspberrypi.(none)>
2015-05-27 17:31:33 -0400

    Modification texte et ajout disclaimer

--- inc/about.php
+++ inc/about.php
@@ -3,7 +3,7 @@
         <h2>A propos du générateur de mots de passe</h2>
     </div>
     <div class="modal-body">
-        <p>Le générateur de mots de passe vous permet de changer rapidement vos mots de passe lorsque vous êtes à court
+        <p>Le générateur de mots de passe vous permet de changer rapidement vos <strong>mots de passe</strong> lorsque vous êtes à court
             d'inspiration et que vous avez oublié vos logins pour la énième fois!! :). C'est comme ça que je l'ai conçut.
 
         <p>Je dois quotidiennement me rappeler de plusieurs mots de passe, et je dois en générer des nouveaux régulièrement.
@@ -11,8 +11,10 @@
         un mot de passe sur site ou on vous demande 10 caractères, avec des chiffres, des lettres, puis des majuscules...?
         Bref, choisir un nouveau mot de passe peut devenir un vrai calvaire sur lequel on déteste perdre du temps.</p>
 
-        <p>J'espère que le générateur de mots de passe vous sera utile. Si vous avez des questions ou des commentaires,
+        <p>J'espère que le <strong>générateur de mots de passe</strong> vous sera utile. Si vous avez des questions ou des commentaires,
         n'hésitez pas en m'en faire part, <a href="http://christophebaldy.net/contact.html" >contactez-moi</a></p>
+
+	<p>Ce site ne récupère aucune données personnelles. Un outils Google Analytics mesure la fréquentation de façon anonyme.</p>
     </div>
     <div class="modal-footer">
         <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
