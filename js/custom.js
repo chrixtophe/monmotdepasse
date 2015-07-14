caea30150be77480c0524077018da0f3f9339d63
Baldy Christophe <christophebaldy@localhost.chrismageia>
2015-07-14 10:25:25 -0400

    modification infos humanstxt

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Lors du clic
+	// Est-ce que des options sont cochées?
+		// Si aucune option n'est séléctionnée, arrêter
+
+	// Est-ce que l'option lettres minuscules est cochée?
+	// Est-ce que l'option lettres majuscules est cochée?
+	// Est-ce que l'otpion chiffres est cochée?
+	// Est-ce que l'otpion caractères spéciaux est cochée?
+	// Combien de caractères doit avoir le chiffre généré?
+
+	// Tableau avec toutes les options
+	// Choix aléatoire de n valeurs dans le tableau
+	// Retourne les n valeurs
+
+	// Si le code fait moins de 6 caractères -> faible
+	// Si le code fait entre 6 et 8 caractères
+		// avec des chiffres, des majuscules et des caractères spéciaux -> moyen
+		// sans chiffres ou sans majuscules ou sans caractères spéciaux -> faible
+	// Si le code fait entre 8 et 10 chiffres
+		// avec des chiffres, des majuscules et des caractères spéciaux -> moyen
+		// sans chiffres ou sans majuscules ou sans caractères spéciaux -> faible
+	// SI le code fait plus de 10 chiffres
+		// avec des chiffres, des majuscules et des caractères spéciaux -> fort
+		// sans chiffres ou sans majuscules ou sans caractères spéciaux -> moyen
